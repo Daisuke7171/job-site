13db672955f0b9d8beaceb1fd09fb4e597c6fd33
Daisuke71 <daisuke.kusano.727@gmail.com>
2025-05-20 11:40:45 +0900

    Update index page with proper Next.js Link

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-// pages/index.js
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
       <h1>ようこそ Job Site!</h1>
       <p>Next.js が動いています 🎉</p>
-      <p>
-        <a href="/jobs">求人一覧へ</a>
-      </p>
+      <ul>
+        <li><Link href="/jobs">求人一覧へ</Link></li>
+        <li><Link href="/companies/list">会社一覧へ</Link></li>
+      </ul>
     </div>
   )
 }
-
