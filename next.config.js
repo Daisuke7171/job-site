8bb566d21fcb5394e2edac70a649033f4a844c54
Daisuke71 <daisuke.kusano.727@gmail.com>
2025-05-20 22:37:56 +0900

    Add output: export to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ← この行が非常に重要です！
-  // もし画像を使っていて、Next.jsのImageコンポーネントを最適化なしで使いたい場合
-  // images: {
-  //   unoptimized: true,
-  // },
-  // 他の設定があれば、それはそのままにします
+  output: 'export', 
 }
 
 module.exports = nextConfig
